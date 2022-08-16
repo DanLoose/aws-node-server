fb64c110a1854bd4409b3c0f6dde6635a1143760
Danilo <danloose14@gmail.com>
2022-08-16 16:34:26 -0300

    app.js setted

--- app.js
+++ app.js
@@ -1,7 +1,20 @@
-const express = require('express')
-const app = express()
+const express = require("express")
+var bodyParser = require("body-parser")
+const router = require("./routes/router")
+
 const PORT = 3333
 
+const app = express()
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+
+// parse application/json
+app.use(bodyParser.json())
+
+app.use("/development", router)
+app.use(express.static("public"))
+
 app.listen(PORT, () => {
-    console.log('Server runnning on port: ' + PORT)
+    console.log("Running on port " + PORT);
 })
