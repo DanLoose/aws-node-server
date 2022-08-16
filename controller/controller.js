e5866995ee86a6e46d76d522e2436d0c8b3cd810
Danilo <danloose14@gmail.com>
2022-08-16 16:37:55 -0300

    routes structure created

--- controller/controller.js
+++ controller/controller.js
@@ -1,8 +1,24 @@
-const crudModel = require("../models/crud-model")
+const model = require("../models/model")
 
 module.exports = {
 
     read: async function (req, res) {
         res.status(200).json('get')
-    }
+    },
+
+    readById: async function (req, res) {
+        res.status(200).json('get')
+    },
+
+    create: async function (req, res) {
+        res.status(200).json('get')
+    },
+
+    update: async function (req, res) {
+        res.status(200).json('get')
+    },
+
+    delete: async function (req, res) {
+        res.status(200).json('get')
+    },
 }
--- routes/router.js
+++ routes/router.js
@@ -2,5 +2,19 @@ const express = require('express')
 const controller = require('../controller/controller')
 const router = express.Router()
 
+// read tasks
+router.get('/', controller.read)
 
-router.get('/', controller.read) 
+// read task by id
+router.get('/:id', controller.readById)
+
+// create task
+router.post('/:id', controller.create)
+
+// update task
+router.put('/:id', controller.update)
+
+// delete task by id
+router.delete('/:id', controller.delete)
+
+module.exports = router
